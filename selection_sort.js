181b164ee03fed73d312374edf9dde8d65e4081a
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-04-19 20:16:17 -0300

    Sort an array of objects using the for loop

--- selection_sort.js
+++ selection_sort.js
@@ -1,16 +1,10 @@
 const livros = require('./lista_livros.js');
 const menorValor = require('./menor_preco.js');
+const troca = require('./troca.js');
 
 for(let atual = 0; atual < livros.length - 1; atual++){
-    let menor_valor = menorValor(livros, atual)
-    
-    let livroAtual = livros[atual]
-    console.log('Livro atual: ', livros[atual])
-    let livroMenorPreco = livros[menor_valor]
-    console.log('Menor preço: ', livros[menor_valor])
-    
-    livros[atual] = livroMenorPreco
-    livros[menor_valor] = livroAtual 
+    let menor_valor = menorValor(livros, atual)   
+    troca(livros, menor_valor)
 }
 
 console.log(livros)
