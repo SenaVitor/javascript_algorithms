a4970bf7e2b69bf3324511c21904fe4015fdbae1
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-04-19 20:14:53 -0300

    Sort an array of objects using for and while loops

--- insertion_sort.js
+++ insertion_sort.js
@@ -1,13 +1,11 @@
 const livros = require('./lista_livros');
+const troca = require('./troca');
 
 function insertionSort(lista){
-    for(let atual = 1; atual < lista.length; atual++){
+    for(let atual = 0; atual < lista.length; atual++){
         let analise = atual;
-        while(lista[analise].preco < lista[analise - 1].preco){
-            let itemAnalise = lista[analise];
-            let itemAnterior = lista[analise - 1];
-            lista[analise] = itemAnterior;
-            lista[analise - 1] = itemAnalise;
+        while(analise > 0 && lista[analise].preco < lista[analise - 1].preco){
+            troca(lista, analise);
             analise --;
         }
     }
