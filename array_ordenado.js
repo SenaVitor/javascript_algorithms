cb1fb6fad8dcd2753b00192457b83e1c714686f0
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-05-16 22:16:44 -0300

    Create a sorted array

---
+++ array_ordenado.js
@@ -0,0 +1,48 @@
+const listaLivros = [
+    {
+      titulo: "Go",
+      preco: 15
+    },
+    {
+      titulo: "C++",
+      preco: 25
+    },
+    {
+      titulo: "Java",
+      preco: 30
+    },
+    {
+    titulo: "PHP",
+    preco: 45
+    },
+    {
+      titulo: "Elixir",
+      preco: 50
+    },
+    {
+      titulo: "Rust",
+      preco: 52
+    },
+    {
+      titulo: "Scala",
+      preco: 60
+    },
+    {
+      titulo: "Ruby",
+      preco: 68
+    },
+    {
+      titulo: "JavaScript",
+      preco: 75
+    },
+    {
+      titulo: "C#",
+      preco: 83
+    },
+    {
+      titulo: "Python",
+      preco: 90
+    },
+  ]
+  
+  module.exports = listaLivros
